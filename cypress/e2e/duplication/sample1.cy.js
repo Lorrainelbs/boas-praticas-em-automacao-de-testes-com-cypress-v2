9286c4092b9577f7f631ea06977595975df75670
lorrainelbs <you@example.com>
2024-05-06 17:48:25 -0300

    first commit

--- cypress/e2e/duplication/sample1.cy.js
+++ cypress/e2e/duplication/sample1.cy.js
@@ -1,5 +1,5 @@
 describe('Code duplication bad practice - repetitive steps', () => {
-  it('searches by typing and hitting enter', () => {
+  beforeEach(() => {
     cy.intercept(
       'GET',
       '**/search**'
@@ -10,8 +10,13 @@ describe('Code duplication bad practice - repetitive steps', () => {
 
     cy.get('input[type="text"]')
       .should('be.visible')
-      .and('have.value', 'redux')
+      .as('searchField') //ascrescentar esse alias
+      .and('have.value', 'redux') //redux é a pesquisa padrão; por default 
       .clear()
+    
+  })
+  it('searches by typing and hitting enter', () => {
+    cy.get('@searchField')
       .type('frontend testing{enter}')
 
     cy.wait('@getStories')
@@ -21,23 +26,12 @@ describe('Code duplication bad practice - repetitive steps', () => {
   })
 
   it('searches by typing and pressing the search button', () => {
-    cy.intercept(
-      'GET',
-      '**/search**'
-    ).as('getStories')
-
-    cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
-
-    cy.get('input[type="text"]')
-      .should('be.visible')
-      .and('have.value', 'redux')
-      .clear()
+    cy.get('@searchField')
       .type('frontend testing')
 
     cy.contains('button', 'Search')
       .should('be.visible')
-      .click()
+      .click() 
 
     cy.wait('@getStories')
 
